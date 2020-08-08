369f1c60fdcc8e91cc81e43b2bef7d6aa2e507d8
Wididou <fw_dekkiche@esi.dz>
2020-08-08 19:54:15 +0100

    use seprate DishdetailComponent

---
+++ confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Card, CardImg,CardImgOverlay, CardText,
+    CardBody,CardTitle } from 'reactstrap';
+
+class Dishdetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderDish(dish) {
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+    renderComments(comments) {
+        if (comments != null) {
+            const commentss = comments.map((comment) => {
+                return(
+                    <li>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author}, {comment.date}</p>
+                    </li>
+                );
+            });
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {commentss}                   
+                    </ul>
+                </div>
+            );
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+    
+    render() {
+        if(this.props.selectedDish != null){
+            return(
+                <div className="row">
+                    { this.renderDish(this.props.selectedDish) } 
+                    { this.renderComments(this.props.selectedDish.comments) }         
+                </div>
+            );
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+}
+export default Dishdetail;
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg,CardImgOverlay, CardText,
     CardBody,CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -9,25 +10,9 @@ class Menu extends Component {
             selectedDish:null
         }
     }
+    
     onDishSelect(dish){
         this.setState({ selectedDish : dish });
-    }
-
-    renderDish(dish){
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
     }
 
     render() {
@@ -50,11 +35,7 @@ class Menu extends Component {
             <div className="row">
                 {menu}
             </div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                </div>
-            </div>
+            <Dishdetail selectedDish={this.state.selectedDish} />
           </div>
         );
     }
