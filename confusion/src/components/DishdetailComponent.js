01271025c1dda8de656afa678147e275898fdb3a
Wididou <fw_dekkiche@esi.dz>
2020-09-15 23:55:43 +0100

    use comment param

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle,
+    Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 function RenderDish({dish}) {
     return(
@@ -21,14 +24,15 @@ function RenderComments({comments}) {
             return(
                 <li>
                     <p>{comment.comment}</p>
-                    <p>-- {comment.author}, 
-                        {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric', 
-                            month: 'short', 
-                            day: '2-digit'}).format(
-                                new Date(Date.parse(comment.date)
-                                )
-                            )}</p>
+                    <p>-- {comment.author}
+                        ,{new Intl.DateTimeFormat('en-US', {
+                                year: 'numeric', 
+                                month: 'short', 
+                                day: '2-digit'}).format(
+                                    new Date(Date.parse(comment.date)
+                                    )
+                                )}
+                    </p>
                 </li>
             );
         });
@@ -52,6 +56,17 @@ const DishDetail = (props) => {
     if(props.dish != null){
         return(
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
                 <div className="row">
                     <RenderDish dish={props.dish} /> 
                     <RenderComments comments={props.comments} /> 
