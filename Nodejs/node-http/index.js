6efc474d5a0af30db7c12688f650d0e062bfd368
Wididou <fw_dekkiche@esi.dz>
2020-09-23 13:53:20 +0100

    init express package

--- Nodejs/node-http/index.js
+++ Nodejs/node-http/index.js
@@ -51,5 +51,5 @@ const server = http.createServer((req, res) => {
 })*/
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);//we use backquotes bcz there's var
 });
