a0948fa7a81be52787e65b258c559df665bfa500
Wididou <fw_dekkiche@esi.dz>
2020-09-24 18:52:51 +0100

    mongoose example part 2

--- Nodejs/node-mongo/index.js
+++ Nodejs/node-mongo/index.js
@@ -6,7 +6,6 @@ const dbname = 'conFusion'; //
 const dboper = require('./operations');
 /* With Promises */
 MongoClient.connect(url).then((client) => {
-
     console.log('Connected correctly to server');
     const db = client.db(dbname);
 
@@ -14,35 +13,27 @@ MongoClient.connect(url).then((client) => {
         "dishes")
         .then((result) => {
             console.log("Insert Document:\n", result.ops);
-
             return dboper.findDocuments(db, "dishes");
         })
         .then((docs) => {
             console.log("Found Documents:\n", docs);
-
             return dboper.updateDocument(db, { name: "Vadonut" },
                     { description: "Updated Test" }, "dishes");
-
         })
         .then((result) => {
             console.log("Updated Document:\n", result.result);
-
             return dboper.findDocuments(db, "dishes");
         })
         .then((docs) => {
-            console.log("Found Updated Documents:\n", docs);
-                            
+            console.log("Found Updated Documents:\n", docs);              
             return db.dropCollection("dishes");
         })
         .then((result) => {
             console.log("Dropped Collection: ", result);
-
             return client.close();
         })
         .catch((err) => console.log(err));
-
-})
-.catch((err) => console.log(err));
+    }).catch((err) => console.log(err));
 
 /* Without Promises */
 /*MongoClient.connect(url, (err, client) => { //callback function
