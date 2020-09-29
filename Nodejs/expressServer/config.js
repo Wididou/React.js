6a408c5bd750e98c4e21ad2cdd1e92f7fa569eb6
Wididou <fw_dekkiche@esi.dz>
2020-09-29 21:54:27 +0100

    using facebook oath

--- Nodejs/expressServer/config.js
+++ Nodejs/expressServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '3003394673099419',
+        clientSecret: 'fb35563b0a03f1d028580a56a4cdb57d'
+    }
 }
